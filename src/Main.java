dc8624c8863366af9395e885d9405938c3b91a1e
mohammad <31886038+mohammadsedghi@users.noreply.github.com>
2023-03-10 17:10:39 +0330

    nothing

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello mohammad and java!");
-          System.out.println("Hello mohammad and java!");
+        System.out.println("");
+          System.out.println("");
     }
 }
